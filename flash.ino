398d9d7ad0eb3832dfa21ca17c01e9bcca187e1d
kromych <kromych@noreply.github.com>
2019-05-26 12:13:55 -0700

    Read EEPROM

--- flash.ino
+++ flash.ino
@@ -8,27 +8,81 @@
 #define SHIFT_CLK   3
 #define SHIFT_LATCH 4
 
-// Set up the board
+#define EEPROM_D0   5
+#define EEPROM_D1   6
+#define EEPROM_D2   7
+#define EEPROM_D3   8
+#define EEPROM_D4   9
+#define EEPROM_D5   10
+#define EEPROM_D6   11
+#define EEPROM_D7   12
 
-void setup()
+/**
+ * Set teh adddress
+ */
+void setAddress(int address, bool outputEnable)
 {
-    // Set up in modes
-
-    pinMode(SHIFT_DATA, OUTPUT);
-    pinMode(SHIFT_CLK, OUTPUT);
-    pinMode(SHIFT_LATCH, OUTPUT);
-
     // Output data to the shift registers
 
-    shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, 0xff);
+    shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, 0x00);
+    shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, 0x00);
+
+    shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, ((address >> 8) & 0xff) | (outputEnable ? 0x00 : 0x80));
+    shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, (address & 0xff));
 
     digitalWrite(SHIFT_LATCH, LOW);
     digitalWrite(SHIFT_LATCH, HIGH);
     digitalWrite(SHIFT_LATCH, LOW);
 }
 
-// Run repeatedly
+/**
+ * Read EEPROM
+ */
+byte readEEPROM(int address)
+{
+    byte data = 0;
 
+    setAddress(address, /*outputEnable*/ true);
+
+    for (int pin = EEPROM_D7; pin >= EEPROM_D0; --pin)
+    {
+        data = (data << 1) | digitalRead(pin);
+    }
+
+    return data;
+} 
+
+/**
+ * Switch data I/O mode
+ */
+void setDataMode(int mode)
+{
+    for (int pin = EEPROM_D7; pin >= EEPROM_D0; --pin)
+    {
+        pinMode(pin, mode);
+    }
+}
+
+/** 
+ * Set up the board
+ */
+void setup()
+{
+    // Set up pin modes
+
+    pinMode(SHIFT_DATA, OUTPUT);
+    pinMode(SHIFT_CLK, OUTPUT);
+    pinMode(SHIFT_LATCH, OUTPUT);
+
+    setDataMode(INPUT);
+
+    Serial.begin(57600); // Had to set that in the Baud Rate dialog, too
+    Serial.println(readEEPROM(0));
+}
+
+/** 
+ * Run repeatedly
+ */
 void loop()
 {
 
