93efd9d6143f271ba14395c541aaaaaa7a01a2bc
kromych <kromych@noreply.github.com>
2019-05-26 12:26:25 -0700

    Dump EEPROM content

--- flash.ino
+++ flash.ino
@@ -77,7 +77,28 @@ void setup()
     setDataMode(INPUT);
 
     Serial.begin(57600); // Had to set that in the Baud Rate dialog, too
-    Serial.println(readEEPROM(0));
+
+    for (int base = 0; base <= 255; base += 16)
+    {
+        byte data[16];
+
+        for (int offset = 0; offset <= 15; ++offset)
+        {
+            data[offset] = readEEPROM(base + offset);
+        }
+
+        char buf[80];
+
+        sprintf(
+            buf, 
+            "%03x:  %02x %02x %02x %02x %02x %02x %02x %02x  %02x %02x %02x %02x %02x %02x %02x %02x",
+            base,
+            data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], 
+            data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15]
+        );
+
+        Serial.println(buf);
+    }
 }
 
 /** 
